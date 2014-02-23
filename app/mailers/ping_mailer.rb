08d4872fbc6e5d19dbf086d3d1eb1e5044a0031b
Alex Levine <a17levine@gmail.com>
2014-02-22 19:01:01 -0600

    finished implementing twilio and email... or so i think...now to test

--- app/mailers/ping_mailer.rb
+++ app/mailers/ping_mailer.rb
@@ -2,8 +2,9 @@ class PingMailer < ActionMailer::Base
   default from: "alfredmakersquare@gmail.com"
 
   def ping(ping_object)
-    @user = ping_object.user
-    @message  = ping_object.message
-    mail(to: @user.sms_email, subject: "")
+    user = ping_object.user
+    name  = ping_object.message
+    message = "#{name} is waiting for you in the Congress lobby. -Alfred"
+    mail(to: user.email, subject: message)
   end
 end
--- app/models/ping.rb
+++ app/models/ping.rb
@@ -2,8 +2,21 @@ class Ping < ActiveRecord::Base
   belongs_to :user
   after_save :send_ping
 
+  @twilio_client = Twilio::REST::Client.new(ENV['TWILIO_SID'],ENV['TWILIO_TOKEN'])
+
   def send_ping
+  	# send email
   	PingMailer.ping(self).deliver
+  	# send sms
+  	send_sms
   end
 
+  def send_sms
+		message = "#{self.message} is waiting for you in the Congress lobby. -Alfred"
+		@twilio_client.account.sms.messages.create(
+			:from => "+1#{ENV['TWILIO_PHONE_NUMBER']}", 
+			:to => user.phone,
+			:body => message
+    )
+	end
 end
